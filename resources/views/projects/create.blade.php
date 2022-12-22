621eec6a42d1bb49c1d7d6b20dcccc858080b8c1
Nikola Perisic <5dzoni5@gmail.com>
2022-12-22 23:57:38 +0100

    Pass test

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,18 +1,29 @@
-{{-- Form without any design --}}
-
-{{-- TASK: add the validation errors here - with whatever HTML structure you want --}}
-{{-- in case of title/description empty, visitor should see --}}
-{{-- "The name field is required." and "The description field is required." --}}
-
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>
+                    {{ $error }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
     Title:
     <br />
     <input type="text" name="title" />
+    @error('title')
+        <small class="alert alert-danger">{{ $message }}</small>
+    @enderror
     <br /><br />
     Description:
     <br />
     <input type="text" name="description" />
+    @error('description')
+        <small class="alert alert-danger">{{ $message }}</small>
+    @enderror
     <br /><br />
     <button type="submit">Save</button>
 </form>
